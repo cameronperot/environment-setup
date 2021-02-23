57ec3adf18d5bd5d7a4a3c678149033dfe4f70d1
Cameron Perot <cameronperot@users.noreply.github.com>
2021-02-23 22:08:16 +0100

    Updated dotfiles

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -6,7 +6,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -P)"
 # copy over dotfiles
 rsync -avz --progress -h $HOME/.zshrc $DIR/dotfiles/.zshrc
 rsync -avz --progress -h $HOME/.tmux.conf $DIR/dotfiles/.tmux.conf
+rsync -avz --progress -h $HOME/.config/htop/ $DIR/dotfiles/.config/htop/
 rsync -avz --progress -h $HOME/.config/nvim/init.vim $DIR/dotfiles/.config/nvim/init.vim
+rsync -avz --progress -h $HOME/.config/i3/config $DIR/dotfiles/.config/i3/config
+rsync -avz --progress -h $HOME/.config/i3status/config $DIR/dotfiles/.config/i3status/config
 rsync -avz --progress -h $HOME/.config/ranger/ $DIR/dotfiles/.config/ranger/
 
 cd $DIR
