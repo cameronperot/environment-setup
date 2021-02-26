795611e81dd7c3f880ac0120025fdc5f75cc1453
Cameron Perot <cameronperot@users.noreply.github.com>
2021-02-26 17:10:55 +0100

    Updated dotfiles

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -6,6 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -P)"
 # copy over dotfiles
 rsync -avz --progress -h $HOME/.zshrc $DIR/dotfiles/.zshrc
 rsync -avz --progress -h $HOME/.tmux.conf $DIR/dotfiles/.tmux.conf
+rsync -avz --progress -h $HOME/.XCompose $DIR/dotfiles/.XCompose
 rsync -avz --progress -h $HOME/.config/htop/ $DIR/dotfiles/.config/htop/
 rsync -avz --progress -h $HOME/.config/nvim/init.vim $DIR/dotfiles/.config/nvim/init.vim
 rsync -avz --progress -h $HOME/.config/i3/config $DIR/dotfiles/.config/i3/config
