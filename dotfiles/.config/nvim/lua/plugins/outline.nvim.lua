45e2afbbf013a671c978da77b99e0a1ed07a6ed2
Cameron Perot <cameronperot@users.noreply.github.com>
2025-04-27 00:23:47 +0200

    Updated dotfiles

--- dotfiles/.config/nvim/lua/plugins/outline.nvim.lua
+++ dotfiles/.config/nvim/lua/plugins/outline.nvim.lua
@@ -2,12 +2,7 @@ local M = {
     "hedyhli/outline.nvim",
     lazy = true,
     cmd = { "Outline", "OutlineOpen" },
-    keys = { -- Example mapping to toggle outline
-        { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
-    },
-    opts = {
-        -- Your setup opts here
-    },
+    opts = {},
 }
 
 return { M }
