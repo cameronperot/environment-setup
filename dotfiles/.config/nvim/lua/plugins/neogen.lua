4b6e0e85949b154b2e7201531a0eb69a7cc32e18
Cameron Perot <cameronperot@users.noreply.github.com>
2025-04-26 23:29:47 +0200

    Updated dotfiles

---
+++ dotfiles/.config/nvim/lua/plugins/neogen.lua
@@ -0,0 +1,25 @@
+local M = {
+    "danymat/neogen",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    ft = {
+        "python",
+        "rust",
+    },
+    cmd = { "Neogen" },
+    config = function()
+        require("neogen").setup({
+            enabled = true,
+            input_after_comment = true,
+            languages = {
+                python = {
+                    template = {
+                        annotation_convention = "reST",
+                    },
+                },
+            },
+        })
+    end,
+    version = "*",
+}
+
+return { M }
