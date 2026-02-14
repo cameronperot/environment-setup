45ea1509cf7b74e52a943082e6c40e0b7d7808f0
Cameron Perot <cameronperot@users.noreply.github.com>
2026-02-14 22:11:36 +0100

    Updated dotfiles

--- dotfiles/.mamba_init.sh
+++ dotfiles/.mamba_init.sh
@@ -1,6 +1,7 @@
 # Micromamba initialization
-export MAMBA_ROOT_PREFIX="${HOME}/.micromamba"
+export MAMBA_ROOT_PREFIX="${MAMBA_ROOT_PREFIX:-${HOME}/.micromamba}"
 export MAMBA_EXE="${MAMBA_ROOT_PREFIX}/bin/micromamba"
+
 __mamba_setup="$("${MAMBA_EXE}" shell hook --shell zsh --root-prefix "${MAMBA_ROOT_PREFIX}" 2>/dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__mamba_setup"
@@ -8,3 +9,11 @@ else
     alias micromamba="${MAMBA_EXE}"
 fi
 unset __mamba_setup
+
+if [ -x "${MAMBA_EXE}" ]; then
+    if [ -d "${MAMBA_ROOT_PREFIX}/envs/dev" ]; then
+        micromamba activate dev
+    else
+        micromamba activate base 2>/dev/null
+    fi
+fi
